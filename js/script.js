fda5ec3b765ed5441d30ee9b2b945ff904c3aa7c
sanketd11 <sanketd11@gmail.com>
2017-11-28 21:03:05 -0700

    Line chart with scroll update

--- js/script.js
+++ js/script.js
@@ -4,30 +4,97 @@ d3.json('data/us-states.json',function(error,data){
    let lineChart = new LineChart()
    let barChart = new BarChart()
    let parallelChart = new ParallelChart()
-
+   let allyearEconomicSectorData = {}
 
    map.drawMap()
-   d3.csv('data/2011_Application_Count.csv', function(error,stateData){
-     map.updateMap(stateData)
-     lineChart.updateLine(stateData)
-   })
-   d3.csv('data/2011_Sector_Wise_Application_Count.csv', function(error,sectorData){
-     barChart.updateBar(sectorData)
-   })
-   yearWise.update()
-   d3.csv('data/2011_all_data.csv', function(error, yearData){
-     // console.log(yearData)
-     // var nested_data = d3.nest()
-     //                  .key(function(d) { return d.employer_name; }).sortKeys(d3.descending)
-     //                  .key(function(d) { return d.pw_job_title_9089; }).sortKeys(d3.descending )
-     //                  .rollup(function(leaves) { return leaves.length; })
-     //                  .entries(yearData);
-     //  console.log(nested_data)
-     parallelChart.updateParallelPlot(yearData)
-   })
+   callLine = function(year,yearCaseCounts){
+       lineChart.updateLine(yearCaseCounts,parseInt(year))
+   }
+   callBarChart= function(economicSectorCaseCounts){
+      barChart.updateBar(economicSectorCaseCounts)
+   }
+   callAllBarChart= function(economicSectorCaseCounts){
+      barChart.updateAllBar(economicSectorCaseCounts)
+   }
+
+   callParallelPlot = function(yearData){
+    parallelChart.updateParallelPlot(yearData)
+  }
+
+   dataInput = function(year){
+     d3.csv('data/'+year+'_all_data.csv', function(error, yearData){
+
+      let economicSectorCaseCounts = d3.nest()
+                              .key(function(d){ return d.US_Economic_Sector;})
+                              .key(function(d){ return d.Case_Status})
+                              .rollup(function(v){ return v.length})
+                              .entries(yearData);
+      let economicSectorCounts = d3.nest()
+                              .key(function(d){ return d.US_Economic_Sector;})
+                              .rollup(function(v){ return v.length})
+                              .entries(yearData);
+
+      let yearCaseCounts = d3.nest()
+                              .key(function(d){ return d.Case_Status;})
+                              .rollup(function(v){ return v.length})
+                              .entries(yearData);
+
+      callLine(year,yearCaseCounts);
+      allyearEconomicSectorData[year] = economicSectorCounts;
+
+     })
+   }
    // Provision for scrolly telling
     $(window).scroll(function(){
-      console.log('SCROLL BODY', $(window).height());
+      // console.log('SCROLL BODY', $(window).height());
       console.log($(window).scrollTop())
+      if (($(window).scrollTop() >=-10 && $(window).scrollTop()<900)){
+        d3.select('#container').classed('navbar-fixed-top',true)
+        d3.select('#container1').classed('navbar-fixed-top', false)
+        d3.select('#container1').classed('show',false)
+        d3.select('#container1').classed('hide',true)
+
+      }else{
+        d3.select('#container').classed('navbar-fixed-top',false)
+        d3.select('#container1').classed('navbar-fixed-top', true)
+        d3.select('#container1').classed('hide',false)
+        d3.select('#container1').classed('show',true)
+
+      }
+      if ($(window).scrollTop() >=-10 && $(window).scrollTop()<150){
+        dataInput ('2011');
+
+      }else if(($(window).scrollTop() >=150 && $(window).scrollTop()<300)){
+        dataInput('2012');
+      }
+      else if(($(window).scrollTop() >=300 && $(window).scrollTop()<450)){
+        dataInput('2013');
+      }
+      else if(($(window).scrollTop() >=450 && $(window).scrollTop()<600)){
+        dataInput('2014');
+      }
+      else if(($(window).scrollTop() >=600 && $(window).scrollTop()<750)){
+        dataInput('2015');
+      }
+      else if(($(window).scrollTop() >=750 && $(window).scrollTop()<900)){
+        dataInput('2016');
+      }else{
+        let data = []
+        for (let key1 in allyearEconomicSectorData) {
+            let counter =0
+            for (let key2 in allyearEconomicSectorData[key1]){
+              let temp = allyearEconomicSectorData[key1][key2]
+              if (!data.hasOwnProperty(counter)) {
+                  data[counter] = { key:temp.key, value:[{key:key1, value: temp.value}]}
+              }else{
+                  data[counter].value.push({key:key1, value : temp.value})
+              }
+              counter+=1
+            }
+
+        }
+        callAllBarChart(data);
+        // callParallelPlot('2011');
+      }
     });
- });
+});
