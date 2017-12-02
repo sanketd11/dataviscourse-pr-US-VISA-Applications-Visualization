3639262ef4d30db9cd6193664861a27a2438a57c
sanketd11 <sanketd11@gmail.com>
2017-12-01 19:59:04 -0700

    Stacked bar chart and line chart update

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ d3.json('data/us-states.json',function(error,data){
    let yearWise = new YearChart(map)
    let lineChart = new LineChart()
    let barChart = new BarChart()
+   let barChart2 = new BarChart2()
    let parallelChart = new ParallelChart()
    let allyearEconomicSectorData = []
    let allyeareconomicSectorCaseCounts=[]
    let allyearCaseCounts=[]
+   let allyearVisaTypeCounts =[]
+   let allyearVisaTypeCaseCounts =[]
    let data_econ = []
+   let data_visaType =[]
    let allyearParallelPlot = []
    let flag = true
 
@@ -20,15 +24,19 @@ d3.json('data/us-states.json',function(error,data){
    callBarChart= function(i){
       barChart.updateBar(allyeareconomicSectorCaseCounts[i].value)
    }
+   callBarChart2= function(i){
+      barChart2.updateBar(allyearVisaTypeCaseCounts[i].value)
+   }
    callAllBarChart= function(data_econ){
       barChart.updateAllBar(data_econ)
    }
-
+   callAllBarChart2= function(data_visaType){
+      barChart2.updateAllBar(data_visaType)
+   }
    callParallelPlot = function(yearData){
-
       parallelChart.updateParallelPlot(yearData)
-
   }
+
   let yearValues = ["2011","2012", "2013","2014","2015","2016"]
   dataInput = function(year){
      d3.csv('data/'+year+'_all_data.csv', function(error, yearData){
@@ -47,12 +55,26 @@ d3.json('data/us-states.json',function(error,data){
                               .key(function(d){ return d.Case_Status;})
                               .rollup(function(v){ return v.length})
                               .entries(yearData);
+      let yearVisaTypeCounts = d3.nest()
+                                .key(function(d){return d.Class_Of_Admission})
+                                .rollup(function(v){ return v.length})
+                                .entries(yearData);
+
+      let yearVisaTypeCaseCounts = d3.nest()
+                              .key(function(d){ return d.Class_Of_Admission;})
+                              .key(function(d){ return d.Case_Status})
+                              .rollup(function(v){ return v.length})
+                              .entries(yearData);
+
+
       let parallelPlotData = getRandom(yearData, 1500)
 
       allyearEconomicSectorData.push({key:year, value:economicSectorCounts});
       allyeareconomicSectorCaseCounts.push({key:year, value:economicSectorCaseCounts});
       allyearCaseCounts.push({key:year, value:yearCaseCounts})
       allyearParallelPlot.push({key:year, value:parallelPlotData})
+      allyearVisaTypeCounts.push({key:year, value:yearVisaTypeCounts})
+      allyearVisaTypeCaseCounts.push({key:year, value:yearVisaTypeCaseCounts})
      })
   }
   function getRandom(arr, n) {
@@ -125,10 +147,29 @@ d3.json('data/us-states.json',function(error,data){
                 counter+=1
               }
             }
+            if (data_visaType.length == 0){
+              let counter2 =0
+              for (let key1=0 ; key1<allyearVisaTypeCounts.length; key1++) {
+                console.log("Key--->",key1)
+                let counter =0
+
+                for (let key2=0; key2 < allyearVisaTypeCounts[key1].value.length; key2++){
+                  let temp = allyearVisaTypeCounts[key1].value[key2]
+                  if (counter2 == 0) {
+                      data_visaType[counter] = {key:temp.key, value:[{key:allyearVisaTypeCounts[key1].key, value: temp.value}]}
+                  }else{
+                      data_visaType[counter].value.push({key:allyearVisaTypeCounts[key1].key, value : temp.value})
+                  }
+                  counter+=1
+                }
+                counter2+=1
+              }
           }
+        }
           console.log("data_econ:--->",data_econ)
           callAllBarChart(data_econ);
-          console.log(allyearParallelPlot[0].key)
+          callAllBarChart2(data_visaType)
+          console.log("Data Vis Type: --->", data_visaType)
           callParallelPlot(allyearParallelPlot[0].value)
           flag = false
         }
